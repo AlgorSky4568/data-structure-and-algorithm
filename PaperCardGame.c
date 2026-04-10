b70fdf485398bf7b84dee5723614f0aae8d9a412
Algorsky <dhy4568527913@163.com>
2026-04-10 16:08:05 +0800

    纸牌游戏完成

---
+++ PaperCardGame.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int n;
+    scanf("%d",&n);
+    int m = n+1;
+    int *cards = (int*)malloc(m * sizeof(int));
+
+    //1为正面，-1为背面
+    for (int i = 0; i < m; i++){
+        cards[i] = 1;
+    }
+
+    for(int i = 2; i < m; i++){
+        for(int j = i; j < m; j +=i){
+            cards[j] = -cards[j];
+        }
+    }
+
+    int total = 0;
+    for(int i = 1; i < m; i++){
+        if(cards[i] == 1){
+            printf("%d ",i);
+            total++;
+        }
+        
+    }
+    printf("\n%d",total);
+}
