6395547ac3d87527447a20e9f406b72c7255e639
Algorsky <dhy4568527913@163.com>
2026-04-11 17:14:19 +0800

    完成逆波兰算法
    收获：
    1. 注意输入的换行
    2. 逆波兰算法对于减和除的顺序有讲究，先出来的是减数和除数，后出来的是被减数和被除数
    3. C中单个数字字符转为数字用ch-'0'，字符串用atoi（）

--- reverseBolan.c
+++ reverseBolan.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 typedef struct {
     int data[20];
     int top;
@@ -20,50 +21,57 @@ void push(stack *s, int data)
 
 void pop(stack *S, int *data)
 {
-    if (S->top != -1) {
-        *data = S->data[S->top];
-        S->top = S->top - 1;
-    }
-
+    *data = S->data[S->top];
+    S->top = S->top - 1;
 }
 
 int main(void) {
-    stack s;
-    int num;
+    stack s; //栈
+    int num; //循环次数
     scanf("%d", &num);
-    initStack(&s);
+    getchar();
+    initStack(&s); //初始化栈
 
-    int result = 0;
-    int num1,num2;
+    int result1 = 0;
+    int result2 = 0;
+    int num1,num2;//进行运算的两个数
+    num1 = 0;
+    num2 = 0;
     while (num != 0) {
-        char ch;
-        scanf("%c", &ch);
-        if (ch == '+') {
+        char ch[20];
+        scanf("%s", ch);
+        getchar();
+        if (ch[0] == '+') {
             pop(&s, &num1);
             pop(&s, &num2);
-            result = num1 + num2;
+            result1 = num1 + num2;
+            push(&s,result1);
         }
-        else if (ch == '-') {
-            pop(&s, &num1);
+        else if (ch[0] == '-') {
             pop(&s, &num2);
-            result = num1 - num2;
+            pop(&s, &num1);
+            result1 = num1 - num2;
+            push(&s,result1);
         }
-        else if (ch == '*') {
+        else if (ch[0] == '*') {
             pop(&s, &num1);
             pop(&s, &num2);
-            result = num1 * num2;
+            result1 = num1 * num2;
+            push(&s,result1);
         }
-        else if (ch == '/') {
-            pop(&s, &num1);
+        else if (ch[0] == '/') {
             pop(&s, &num2);
-            result = num1 / num2;
+            pop(&s, &num1);
+            result1 = num1 / num2;
+            push(&s,result1);
         }
         else {
-            push(&s, ch - '0');
+            push(&s, atoi(ch));
         }
         num--;
     }
-    printf("%d", result);
+    result2 = s.data[s.top];
+    printf("%d", result2);
 
     return 0;
 }
